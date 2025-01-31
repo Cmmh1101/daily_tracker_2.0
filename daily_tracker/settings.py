633cf292e9ad22de6b09b2c4145abf82415faa40
Carla Montano <cm.emprendiendo@gmail.com>
2025-01-30 22:31:34 -0600

    fixed tailwind

--- daily_tracker/settings.py
+++ daily_tracker/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     "django.contrib.admin",
+    "node",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
